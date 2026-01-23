dafe2e291b28662d11b3c27756fd632a0b58ffec
JinSooo <kimjinso@qq.com>
2026-01-23 21:59:41 +0200

    feat: use easypack for material

---
+++ packages/dashboard/basic/text/easypack.config.ts
@@ -0,0 +1,11 @@
+/**
+ * @easy-editor/easypack configuration
+ * @type {import('@easy-editor/easypack').EasypackConfig}
+ */
+export default {
+  preset: 'material',
+  // globalName 自动从 package.json 推导
+  dev: {
+    port: 5001,
+  },
+}
